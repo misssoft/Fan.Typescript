de0bb6497ba6c7009271f36e61553e8a9ea854c5
Fan Yang <fan.yang.turner@live.co.uk>
2017-01-30 14:21:33 +0000

    data-bind coin via image attribute

--- src/js/app.js
+++ src/js/app.js
@@ -20,6 +20,7 @@ var VendingMachine = (function () {
     function VendingMachine() {
         var _this = this;
         this.paid = ko.observable(0);
+        this.acceptedCoins = [new Quarter()];
         this.acceptCoin = function (coin) {
             var oldTotal = _this.paid();
             _this.paid(oldTotal + coin.Value);
--- src/ts/vendingmachine.ts
+++ src/ts/vendingmachine.ts
@@ -2,6 +2,7 @@
 
 class VendingMachine{
     private paid = ko.observable(0);
+    acceptedCoins: Quarter[] = [new Quarter()]
     acceptCoin = (coin:Quarter): void => {
         let oldTotal = this.paid();
         this.paid(oldTotal + coin.Value);
