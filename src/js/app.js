921679ddad25de0eb42de935c52b4bc6584c6569
Fan Yang <fan.yang.turner@live.co.uk>
2017-01-30 15:17:12 +0000

    Add pay function

--- src/js/app.js
+++ src/js/app.js
@@ -63,11 +63,30 @@ var VendingMachine = (function () {
     function VendingMachine() {
         var _this = this;
         this.paid = ko.observable(0);
+        this.selectedCell = ko.observable(new Cell(new CocaCola()));
         this.cells = ko.observableArray([]);
         this.acceptedCoins = [new Quarter()];
+        this.canPay = ko.pureComputed(function () {
+            return _this.paid() - _this.selectedCell().product.price >= 0;
+        });
+        this.select = function (cell) {
+            cell.sold(false);
+            _this.selectedCell(cell);
+        };
         this.acceptCoin = function (coin) {
             var oldTotal = _this.paid();
             _this.paid(oldTotal + coin.Value);
+        };
+        this.pay = function () {
+            if (_this.selectedCell().stock() < 1) {
+                alert("I'm sorry, we're out of them!");
+                return;
+            }
+            var currentPayed = _this.paid();
+            _this.paid(Math.round(((currentPayed - _this.selectedCell().product.price) * 100)) / 100);
+            var currentStock = _this.selectedCell().stock();
+            _this.selectedCell().stock(currentStock - 1);
+            _this.selectedCell().sold(true);
         };
     }
     Object.defineProperty(VendingMachine.prototype, "size", {
--- src/ts/vendingmachine.ts
+++ src/ts/vendingmachine.ts
@@ -20,9 +20,11 @@ class Cell{
 
 class VendingMachine{
     paid = ko.observable(0);
+    selectedCell = ko.observable(new Cell(new CocaCola()));
     cells = ko.observableArray([]);
     acceptedCoins: Quarter[] = [new Quarter()]
-
+    canPay = ko.pureComputed(()=>
+        this.paid() - this.selectedCell().product.price >=0)
     set size(givenSize:VendingMachineSize){
         this.cells([]);
         for (let index=0; index<givenSize; index++){
@@ -30,8 +32,27 @@ class VendingMachine{
             this.cells.push(new Cell(product));
         }
     }
+
+    select = (cell: Cell): void=>{
+        cell.sold(false);
+        this.selectedCell(cell);
+    }
+
     acceptCoin = (coin:Quarter): void => {
         let oldTotal = this.paid();
         this.paid(oldTotal + coin.Value);
+    }
+
+    pay = (): void => {               
+        if (this.selectedCell().stock() < 1) {
+            alert("I'm sorry, we're out of them!")
+            return;
+        }
+        
+        let currentPayed = this.paid();
+        this.paid(Math.round(((currentPayed - this.selectedCell().product.price) * 100))/100);
+        let currentStock = this.selectedCell().stock();
+        this.selectedCell().stock(currentStock-1);
+        this.selectedCell().sold(true);
     }
 }
